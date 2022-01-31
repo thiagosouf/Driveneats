2891c891b2ec66209938fe3b0cfd9c44cf74f49f
Thiago <thiagosouf@gmail.com>
2022-01-31 11:50:32 -0300

    projeto funcionando

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-
 let x
 let y
 let z
-let precoPrato, precoBebida, precoSobremesa, precoTotal;
+let precoPrato, nomePrato, precoBebida, nomeBebida, precoSobremesa, nomeSobremesa, precoTotal, somaPrato, somaBebida, somaSobremesa;
 function prato(valor){
     document.getElementById("prato-1").classList.remove("selecionado");
     document.getElementById("prato-2").classList.remove("selecionado");
@@ -18,6 +17,7 @@ function prato(valor){
         const check = document.querySelector("#prato-1 > .checkmark-circle");
         check.classList.add("check");
         precoPrato = document.querySelector("#prato-1 > .preco").innerHTML;
+        nomePrato = document.querySelector("#prato-1 > .nome").innerHTML;
         
         
     }
@@ -27,6 +27,7 @@ function prato(valor){
         const check = document.querySelector("#prato-2 > .checkmark-circle");
         check.classList.add("check");
         precoPrato = document.querySelector("#prato-2 > .preco").innerHTML;
+        nomePrato = document.querySelector("#prato-2 > .nome").innerHTML;
         
     }
     if(valor == 3){
@@ -35,6 +36,7 @@ function prato(valor){
         const check = document.querySelector("#prato-3 > .checkmark-circle");
         check.classList.add("check");
         precoPrato = document.querySelector("#prato-3 > .preco").innerHTML;
+        nomePrato = document.querySelector("#prato-3 > .nome").innerHTML;
     }
     x=true
     validar()
@@ -54,6 +56,8 @@ function bebida(valor){
         const check = document.querySelector("#bebida-1 > .checkmark-circle");
         check.classList.add("check");
         precoBebida = document.querySelector("#bebida-1 > .preco").innerHTML;
+        nomeBebida = document.querySelector("#bebida-1 > .nome").innerHTML;
+
         
     }
     if (valor == 2){
@@ -62,6 +66,7 @@ function bebida(valor){
         const check = document.querySelector("#bebida-2 > .checkmark-circle");
         check.classList.add("check");
         precoBebida = document.querySelector("#bebida-2 > .preco").innerHTML;
+        nomeBebida = document.querySelector("#bebida-2 > .nome").innerHTML;
         
         
     }
@@ -71,6 +76,7 @@ function bebida(valor){
         const check = document.querySelector("#bebida-3 > .checkmark-circle");
         check.classList.add("check");
         precoBebida = document.querySelector("#bebida-3 > .preco").innerHTML;
+        nomeBebida = document.querySelector("#bebida-3 > .nome").innerHTML;
     }
     y=true
     validar()
@@ -90,6 +96,7 @@ function sobremesa(valor){
         const check = document.querySelector("#sobremesa-1 > .checkmark-circle");
         check.classList.add("check");
         precoSobremesa = document.querySelector("#sobremesa-1 > .preco").innerHTML;
+        nomeSobremesa = document.querySelector("#sobremesa-1 > .nome").innerHTML;
     }
     if (valor == 2){
         const minhaSobremesa = document.getElementById("sobremesa-2")
@@ -97,6 +104,7 @@ function sobremesa(valor){
         const check = document.querySelector("#sobremesa-2 > .checkmark-circle");
         check.classList.add("check");
         precoSobremesa = document.querySelector("#sobremesa-2 > .preco").innerHTML;
+        nomeSobremesa = document.querySelector("#sobremesa-1 > .nome").innerHTML;
     }
     if(valor == 3){
         const minhaSobremesa = document.getElementById("sobremesa-3")
@@ -104,6 +112,7 @@ function sobremesa(valor){
         const check = document.querySelector("#sobremesa-3 > .checkmark-circle");
         check.classList.add("check");
         precoSobremesa = document.querySelector("#sobremesa-3 > .preco").innerHTML;
+        nomeSobremesa = document.querySelector("#sobremesa-1 > .nome").innerHTML;
     }
     z=true
     validar()
@@ -115,8 +124,39 @@ function validar(){
         botaoPedido.classList.remove("disable")
         botaoPedido.classList.add("enable")
         botaoPedido.innerHTML = "Fechar pedido";
+        document.querySelector(".botao-confirmar").disabled = false;
      }
 }
 
+function confirmar(){
+    document.querySelector(".confirmar").classList.remove("disable-carrinho")
+    document.querySelector(".primeiro-item > p:first-child").innerHTML = nomePrato;
+    document.querySelector(".segundo-item > p:first-child").innerHTML = nomeBebida;
+    document.querySelector(".terceiro-item > p:first-child").innerHTML = nomeSobremesa;
+    document.querySelector(".primeiro-item > p:last-child").innerHTML = precoPrato;
+    document.querySelector(".segundo-item > p:last-child").innerHTML = precoBebida;
+    document.querySelector(".terceiro-item > p:last-child").innerHTML = precoSobremesa;
+    somaBebida = precoBebida.replace('R$ ','')
+    somaBebida = somaBebida.replace(',','.')
+    somaPrato = precoPrato.replace('R$ ','')
+    somaPrato = somaPrato.replace(',','.')
+    somaSobremesa = precoSobremesa.replace('R$ ','')
+    somaSobremesa = somaSobremesa.replace(',','.')
+    somaTotal = (parseFloat(somaBebida) + parseFloat(somaPrato) + parseFloat(somaSobremesa)).toFixed(2)
+    somaTotal = String(somaTotal)
+    somaTotal = somaTotal.replace('.',',')
+    document.querySelector(".total-item > p:last-child").innerHTML = 'R$ ' + somaTotal;
+}
+
+function cadastrar(){
+    let nome = prompt("qual seu nome?")
+    let endereco = prompt("qual seu endereço?")
+    let mensagem ="Olá, gostaria de fazer o pedido:\n   - Prato: " + nomePrato + "\n   - Bebida: " + nomeBebida +"\n   - Sobremesa: " + nomeSobremesa + "\n   Total: R$ " + somaTotal + "\n\n *NOME:* " + nome + "\n *ENDEREÇO:* " + endereco
+    window.open("https://wa.me/+5521920187161?text=" + encodeURIComponent(mensagem)) 
+}
+
+function cancelar(){
+    document.querySelector(".confirmar").classList.add("disable-carrinho")
+}
 
 
